5728dce2c665554d51d5092d08e8341fc7a5607b
Sebastian Vivas <sebastian@avilatek.dev>
2022-04-15 19:39:48 -0400

    placeholder to my nfts page to fix deploy

---
+++ frontend/pages/app/mynfts/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../../components/Layout';
+
+export default function index() {
+  return (
+    <Layout>
+      <div className="min-h-screen min-w-full">
+        <h2>Placeholder</h2>
+      </div>
+    </Layout>
+  );
+}
