dd90dc07b50d22a191619808e86e0fbfffa30d4e
Edward <edwards.svo@gmail.com>
2022-04-19 21:48:21 -0400

    update NFT Contract metadata

--- blockchain/NEARprotocol/nft-mintos-contract/src/lib.rs
+++ blockchain/NEARprotocol/nft-mintos-contract/src/lib.rs
@@ -75,8 +75,8 @@ impl Contract {
             owner_id,
             NFTContractMetadata {
                 spec: "nft-1.0.0".to_string(),
-                name: "NFT Tutorial Contract".to_string(),
-                symbol: "GOTEAM".to_string(),
+                name: "Mintos Marketplace".to_string(),
+                symbol: "MintosNFT".to_string(),
                 icon: None,
                 base_uri: None,
                 reference: None,
