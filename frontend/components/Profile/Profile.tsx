06b1887d5c558a2f53da363d0c8df1a026ef7619
Sebastian Vivas <svivas@correo.unimet.edu.ve>
2022-06-06 15:53:45 -0400

    fixed bug that wouldnt load the profile page for new accounts

--- frontend/components/Profile/Profile.tsx
+++ frontend/components/Profile/Profile.tsx
@@ -25,6 +25,7 @@ export default function Profile() {
   const [marketTokens, setMarketTokens] = React.useState<Array<WholeToken>>([]);
   const [walletTokens, setWalletTokens] = React.useState<Array<WholeToken>>([]);
   const [filter, setFilter] = React.useState(0);
+  const [loaded, setLoaded] = React.useState(false);
 
   const setStorageInNEAR = (e: any) => {
     const amount = Number(e) * ONE_NEAR_IN_YOCTO;
@@ -153,6 +154,13 @@ export default function Profile() {
     getWalletTokens();
   }, [sales]);
 
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoaded(true);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
       {tokens.length > 0 ? (
@@ -167,7 +175,7 @@ export default function Profile() {
             </div>
             <div className="flex flex-col justify-center align-middle items-center self-center p-3 text-lg lg:border-2 rounded-lg  lg:w-1/2 shadow-2xl">
               <div className="w-full lg:flex lg:justify-between">
-                <div className='lg:w-1/2 lg:p-6'>
+                <div className="lg:w-1/2 lg:p-6">
                   <div className="text-4xl font-semibold text-center mb-5">
                     User Data
                   </div>
@@ -180,7 +188,9 @@ export default function Profile() {
                     <div>Comming Soon</div>
                   </div>
                   <div className="flex justify-between w-full pb-2">
-                    <div className="font-semibold text-figma-100">Artist Name </div>{' '}
+                    <div className="font-semibold text-figma-100">
+                      Artist Name{' '}
+                    </div>{' '}
                     <div>Comming Soon</div>
                   </div>
                   <div className="flex justify-between w-full pb-2">
@@ -192,11 +202,13 @@ export default function Profile() {
                     <div>Comming Soon</div>
                   </div>
                   <div className="flex justify-between w-full pb-2">
-                    <div className="font-semibold text-figma-100">Init Date</div>{' '}
+                    <div className="font-semibold text-figma-100">
+                      Init Date
+                    </div>{' '}
                     <div>Comming Soon</div>
                   </div>
                 </div>
-                <div className='lg:w-1/2 lg:p-6'>
+                <div className="lg:w-1/2 lg:p-6">
                   <div className="text-4xl font-semibold mb-5 text-center mt-5 lg:mt-0">
                     Market Data
                   </div>
@@ -220,7 +232,7 @@ export default function Profile() {
                   </div>
                   <div className="flex justify-between w-full pb-2">
                     <div className="font-semibold text-figma-100">
-                      Total Profit {' '}
+                      Total Profit{' '}
                     </div>{' '}
                     <div>Comming Soon</div>
                   </div>
@@ -232,26 +244,26 @@ export default function Profile() {
                     <div className="font-light">Reserved Storage</div>{' '}
                     <div className="font-bold"> {storage} NEAR Ⓝ</div>
                   </div>
-              <div className="text-center mt-8 border-2 p-6">
-                <p>Reserve Storage for Minting</p>
-                <div className="flex justify-center align-middle items-center">
-                  <input
-                    type="text"
-                    placeholder="Reserve Storage Ⓝ"
-                    onChange={(e) => {
-                      setStorageInNEAR(e.target.value);
-                    }}
-                    className="mr-6"
-                  />
-                  <button
-                    type="button"
-                    onClick={() => addStorageDeposit()}
-                    className=" text-center bg-figma-100 rounded-full w-9 h-9 text-white hover:bg-figma-900"
-                  >
-                    +
-                  </button>
-                </div>
-              </div>
+                  <div className="text-center mt-8 border-2 p-6">
+                    <p>Reserve Storage for Minting</p>
+                    <div className="flex justify-center align-middle items-center">
+                      <input
+                        type="text"
+                        placeholder="Reserve Storage Ⓝ"
+                        onChange={(e) => {
+                          setStorageInNEAR(e.target.value);
+                        }}
+                        className="mr-6"
+                      />
+                      <button
+                        type="button"
+                        onClick={() => addStorageDeposit()}
+                        className=" text-center bg-figma-100 rounded-full w-9 h-9 text-white hover:bg-figma-900"
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -316,9 +328,140 @@ export default function Profile() {
           </div>
         </div>
       ) : (
-        <div className="flex justify-center w-full">
-          <Loader />
-        </div>
+        <>
+          {loaded ? (
+            <div>
+              <div className="min-h-screen min-w-full mb-20">
+                <div className="p-4 flex flex-col">
+                  <div className="w-full my-7">
+                    <img
+                      src="/profile.png"
+                      alt="profile"
+                      className="w-40 lg:w-60 mx-auto"
+                    />
+                  </div>
+                  <div className="flex flex-col justify-center align-middle items-center self-center p-3 text-lg lg:border-2 rounded-lg  lg:w-1/2 shadow-2xl">
+                    <div className="w-full lg:flex lg:justify-between">
+                      <div className="lg:w-1/2 lg:p-6">
+                        <div className="text-4xl font-semibold text-center mb-5">
+                          User Data
+                        </div>
+                        <div className="flex justify-between w-full pb-2">
+                          <div className="font-semibold text-figma-100">
+                            User
+                          </div>{' '}
+                          <div className="font-bold">{username}</div>
+                        </div>
+                        <div className="flex justify-between w-full pb-2">
+                          <div className="font-semibold text-figma-100">
+                            Email{' '}
+                          </div>{' '}
+                          <div>Comming Soon</div>
+                        </div>
+                        <div className="flex justify-between w-full pb-2">
+                          <div className="font-semibold text-figma-100">
+                            Artist Name{' '}
+                          </div>{' '}
+                          <div>Comming Soon</div>
+                        </div>
+                        <div className="flex justify-between w-full pb-2">
+                          <div className="font-semibold text-figma-100">
+                            Role
+                          </div>{' '}
+                          <div>Comming Soon</div>
+                        </div>
+                        <div className="flex justify-between w-full pb-2">
+                          <div className="font-semibold text-figma-100">
+                            Range
+                          </div>{' '}
+                          <div>Comming Soon</div>
+                        </div>
+                        <div className="flex justify-between w-full pb-2">
+                          <div className="font-semibold text-figma-100">
+                            Init Date
+                          </div>{' '}
+                          <div>Comming Soon</div>
+                        </div>
+                      </div>
+                      <div className="lg:w-1/2 lg:p-6">
+                        <div className="text-4xl font-semibold mb-5 text-center mt-5 lg:mt-0">
+                          Market Data
+                        </div>
+                        <div className="flex justify-between w-full pb-2">
+                          <div className="font-semibold text-figma-100">
+                            NFTs Minted{' '}
+                          </div>{' '}
+                          <div>Comming Soon</div>
+                        </div>
+                        <div className="flex justify-between w-full pb-2">
+                          <div className="font-semibold text-figma-100">
+                            NFTs Sold{' '}
+                          </div>{' '}
+                          <div>Comming Soon</div>
+                        </div>
+                        <div className="flex justify-between w-full pb-2">
+                          <div className="font-semibold text-figma-100">
+                            Total Sales{' '}
+                          </div>{' '}
+                          <div>Comming Soon</div>
+                        </div>
+                        <div className="flex justify-between w-full pb-2">
+                          <div className="font-semibold text-figma-100">
+                            Total Profit{' '}
+                          </div>{' '}
+                          <div>Comming Soon</div>
+                        </div>
+                        <div className="flex justify-between w-full lg:hidden mt-5 border-t-2 pt-5">
+                          <div className="font-light">Available Balance </div>{' '}
+                          <div className="font-bold">{balance} NEAR Ⓝ</div>
+                        </div>
+                        <div className="flex justify-between w-full">
+                          <div className="font-light">Reserved Storage</div>{' '}
+                          <div className="font-bold"> {storage} NEAR Ⓝ</div>
+                        </div>
+                        <div className="text-center mt-8 border-2 p-6">
+                          <p>Reserve Storage for Minting</p>
+                          <div className="flex justify-center align-middle items-center">
+                            <input
+                              type="text"
+                              placeholder="Reserve Storage Ⓝ"
+                              onChange={(e) => {
+                                setStorageInNEAR(e.target.value);
+                              }}
+                              className="mr-6"
+                            />
+                            <button
+                              type="button"
+                              onClick={() => addStorageDeposit()}
+                              className=" text-center bg-figma-100 rounded-full w-9 h-9 text-white hover:bg-figma-900"
+                            >
+                              +
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-6 px-4 flex justify-between mb-2 md:px-9">
+                    <h2 className="font-semibold text-2xl">My NFTs</h2>
+                  </div>
+                  <div className="flex w-full justify-start px-4 mb-2 md:px-9 lg:px-8">
+                    <div>
+                      <FilterDropdown filter={filter} setFilter={setFilter} />
+                    </div>
+                  </div>
+                  <div className="w-full flex justify-center align-middle text-2xl">
+                    <p>No NFTs yet</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="flex justify-center w-full">
+              <Loader />
+            </div>
+          )}
+        </>
       )}
     </div>
   );
